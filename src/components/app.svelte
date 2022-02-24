d252797a604106ee13cb7adf24dae4407dcbb585
Mancuso Antonio <espedito.mancuso@istitutotagnelli.it>
2022-02-24 19:07:32 +0100

    cleanup and login page

--- src/components/app.svelte
+++ src/components/app.svelte
@@ -1,56 +1,62 @@
 <App { ...f7params } themeDark>
 
-
-  <!-- Views/Tabs container -->
-  <Views tabs class="safe-areas">
-    <!-- Tabbar for switching views-tabs -->
-    <Toolbar tabbar labels bottom>
-      <Link tabLink="#view-home" tabLinkActive iconIos="f7:house_fill" iconAurora="f7:house_fill" iconMd="material:home" text="Home" />
-      <Link tabLink="#view-storico" iconIos="f7:clock" iconAurora="f7:clock" iconMd="f7:clock" text="Storico" />
-      <Link tabLink="#view-settings" iconIos="f7:cart" iconAurora="f7:cart" iconMd="f7:cart" text="Ordini" />
-    </Toolbar>
-
-    <!-- Your main view/tab, should have "view-main" class. It also has "tabActive" prop -->
-    <View id="view-home" main tab tabActive url="/" />
-
-    <!-- Catalog View -->
-    <View id="view-storico" name="storico" tab url="/storico/" />
-
-    <!-- Settings View -->
-    <View id="view-settings" name="settings" tab url="/settings/" />
-
-  </Views>
-
-
-  
-  <LoginScreen id="my-login-screen">
+  <!-- Left panel with cover effect-->
+  <!-- <Panel left cover themeDark>
     <View>
-      <Page loginScreen>
-        <LoginScreenTitle>Login</LoginScreenTitle>
-        <List form>
-          <ListInput
-            type="text"
-            name="username"
-            placeholder="Your username"
-            bind:value={username}
-          />
-          <ListInput
-            type="password"
-            name="password"
-            placeholder="Your password"
-            bind:value={password}
-          />
-        </List>
-        <List>
-          <ListButton title="Sign In" onClick={() => alertLoginData()} />
-        </List>
-        <BlockFooter>
-          Some text about login information.<br />Click "Sign In" to close Login Screen
-        </BlockFooter>
+      <Page>
+        <Navbar title="Left Panel"/>
+        <Block>Left panel content goes here</Block>
       </Page>
     </View>
-  </LoginScreen>
+  </Panel>
+ -->
+
+  <!-- Right panel with reveal effect-->
+  <!-- <Panel right reveal themeDark>
+    <View>
+      <Page>
+        <Navbar title="Right Panel"/>
+        <Block>Right panel content goes here</Block>
+      </Page>
+    </View>
+  </Panel> -->
+
+
+{#if $user_auth == "false"}
+  <View main class="safe-areas" url="/login/" />
+{:else}
+<Views tabs class="safe-areas">
+  <Toolbar tabbar labels bottom>
+    <Link tabLink="#view-home" tabLinkActive iconIos="f7:house_fill" iconAurora="f7:house_fill" iconMd="material:home" text="Home" />
+    <Link tabLink="#view-storico" iconIos="f7:clock" iconAurora="f7:clock" iconMd="f7:clock" text="Storico" />
+    <Link tabLink="#view-ordini" iconIos="f7:cart" iconAurora="f7:cart" iconMd="f7:cart" text="Ordini" />
+  </Toolbar>
+  <View id="view-home" main tab tabActive url="/" />
+  <View id="view-storico" name="catalog" tab url="/storico/" />
+  <View id="view-ordini" name="settings" tab url="/ordini/" />
+</Views>
+{/if}
+
+
+  <!-- Popup -->
+  <!-- <Popup id="my-popup">
+    <View>
+      <Page>
+        <Navbar title="Popup">
+          <NavRight>
+            <Link popupClose>Close</Link>
+          </NavRight>
+        </Navbar>
+        <Block>
+          <p>Popup content goes here.</p>
+        </Block>
+      </Page>
+    </View>
+  </Popup> -->
+
+
 </App>
+
 <script>
   import { onMount } from 'svelte';
 
@@ -81,11 +87,12 @@
 
   import routes from '../js/routes';
   import store from '../js/store';
+  import { user_auth } from './user_store.svelte';
 
-
+  
   // Framework7 Parameters
   let f7params = {
-    name: 'My App', // App name
+    name: 'snAcks', // App name
     theme: 'auto', // Automatic theme detection
 
 
@@ -94,6 +101,10 @@
     store: store,
     // App routes
     routes: routes,
+    // Register service worker (only on production build)
+    serviceWorker: process.env.NODE_ENV ==='production' ? {
+      path: '/service-worker.js',
+    } : {},
   };
   // Login screen demo data
   let username = '';
