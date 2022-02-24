13de83acd138ea47b7f6e917a3cdb74e7ed7ab46
Antonio Mancuso <mancusoa74@gmail.com>
2022-02-24 19:07:32 +0100

    cleanup and login page

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,13 +1,7 @@
-
 import HomePage from '../pages/home.svelte';
-import AboutPage from '../pages/about.svelte';
-import FormPage from '../pages/form.svelte';
 import StoricoPage from '../pages/storico.svelte';
-import ProductPage from '../pages/product.svelte';
-import SettingsPage from '../pages/settings.svelte';
-
-import DynamicRoutePage from '../pages/dynamic-route.svelte';
-import RequestAndLoad from '../pages/request-and-load.svelte';
+import OrdiniPage from '../pages/ordini.svelte';
+import LoginPage from '../pages/login.svelte';
 import NotFoundPage from '../pages/404.svelte';
 
 var routes = [
@@ -16,77 +10,17 @@ var routes = [
     component: HomePage,
   },
   {
-    path: '/about/',
-    component: AboutPage,
-  },
-  {
-    path: '/form/',
-    component: FormPage,
+    path: '/login/',
+    component: LoginPage,
   },
   {
     path: '/storico/',
     component: StoricoPage,
   },
   {
-    path: '/product/:id/',
-    component: ProductPage,
-  },
-  {
-    path: '/settings/',
-    component: SettingsPage,
-  },
-
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    component: DynamicRoutePage,
-  },
-  {
-    path: '/request-and-load/user/:userId/',
-    async: function ({ router, to, resolve }) {
-      // App instance
-      var app = router.app;
-
-      // Show Preloader
-      app.preloader.show();
-
-      // User ID from request
-      var userId = to.params.userId;
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            component: RequestAndLoad,
-          },
-          {
-            props: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
-    },
-  },
+    path: '/ordini/',
+    component: OrdiniPage,
+  },  
   {
     path: '(.*)',
     component: NotFoundPage,
