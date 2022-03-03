f6314a79d77483e9b7c2fc71111177cf2a6655bd
Andrea Canale <99402143+Andrea-Canale@users.noreply.github.com>
2022-03-03 17:07:44 +0100

    Aggiunto il progetto di capacitor e ho risolto i conflitti nel app.svelte (#3)
    * add gitignore
    
    * cleanup
    
    * fix css
    
    * clean app.svelte and add capacitor project
    
    Co-authored-by: Andrea Canale <andrehackintoshbook@MacBook-Pro-di-Andrea.local>
    Co-authored-by: Andrea055 <vivanco11.ac@gmail.com>

--- src/components/app.svelte
+++ src/components/app.svelte
@@ -1,69 +1,65 @@
 <App { ...f7params } themeDark>
 
-{#if $user_auth == "false"}
-  <View main class="safe-areas" url="/login/" />
-{:else}
-<Views tabs class="safe-areas">
-  <Toolbar tabbar labels bottom>
-    <Link tabLink="#view-home" tabLinkActive iconIos="f7:house_fill" iconAurora="f7:house_fill" iconMd="material:home" text="Home" />
-    <Link tabLink="#view-storico" iconIos="f7:clock" iconAurora="f7:clock" iconMd="f7:clock" text="Storico" />
-    <Link tabLink="#view-ordini" iconIos="f7:cart" iconAurora="f7:cart" iconMd="f7:cart" text="Ordini" />
-  </Toolbar>
-  <View id="view-home" main tab tabActive url="/" />
-  <View id="view-storico" name="catalog" tab url="/storico/" />
-  <View id="view-ordini" name="settings" tab url="/ordini/" />
-</Views>
-{/if}
-</App>
-
+  {#if $user_auth == "false"}
+    <View main class="safe-areas" url="/login/" />
+  {:else}
+  <Views tabs class="safe-areas">
+    <Toolbar tabbar labels bottom>
+      <Link tabLink="#view-home" tabLinkActive iconIos="f7:house_fill" iconAurora="f7:house_fill" iconMd="material:home" text="Home" />
+      <Link tabLink="#view-storico" iconIos="f7:clock" iconAurora="f7:clock" iconMd="f7:clock" text="Storico" />
+      <Link tabLink="#view-ordini" iconIos="f7:cart" iconAurora="f7:cart" iconMd="f7:cart" text="Ordini" />
+    </Toolbar>
+    <View id="view-home" main tab tabActive url="/" />
+    <View id="view-storico" name="catalog" tab url="/storico/" />
+    <View id="view-ordini" name="settings" tab url="/ordini/" />
+  </Views>
+  {/if}
+  </App>
 <script>
   import { onMount } from 'svelte';
-
+  import { getDevice }  from 'framework7/lite-bundle';
+  import { user_auth } from './user_store.svelte';
   import {
     f7,
     f7ready,
     App,
-    Panel,
-    Views,
     View,
-    Popup,
-    Page,
-    Navbar,
+    Views,
     Toolbar,
-    NavRight,
-    Link,
-    Block,
-    BlockTitle,
-    LoginScreen,
-    LoginScreenTitle,
-    List,
-    ListItem,
-    ListInput,
-    ListButton,
-    BlockFooter
+    Link
   } from 'framework7-svelte';
 
-
+  import capacitorApp from '../js/capacitor-app';
   import routes from '../js/routes';
   import store from '../js/store';
-  import { user_auth } from './user_store.svelte';
 
-  
+  const device = getDevice();
   // Framework7 Parameters
   let f7params = {
     name: 'snAcks', // App name
     theme: 'auto', // Automatic theme detection
 
 
-
+    id: 'io.ats.snacks', // App bundle ID
     // App store
     store: store,
     // App routes
     routes: routes,
+
+    // Input settings
+    input: {
+      scrollIntoViewOnFocus: device.capacitor,
+      scrollIntoViewCentered: device.capacitor,
+    },
     // Register service worker (only on production build)
     serviceWorker: process.env.NODE_ENV ==='production' ? {
       path: '/service-worker.js',
     } : {},
+    // Capacitor Statusbar settings
+    statusbar: {
+      iosOverlaysWebView: true,
+      androidOverlaysWebView: false,
+    },
   };
   // Login screen demo data
   let username = '';
@@ -77,7 +73,10 @@
   onMount(() => {
     f7ready(() => {
 
-
+      // Init capacitor APIs (see capacitor-app.js)
+      if (f7.device.capacitor) {
+        capacitorApp.init(f7);
+      }
       // Call F7 APIs here
     });
   })
