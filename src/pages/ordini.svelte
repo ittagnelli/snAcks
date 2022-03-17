cf2d30b2038bdec878adae73b75f69ae5a812ec6
Antonio Mancuso <mancusoa74@gmail.com>
2022-03-17 18:48:21 +0100

    Feature/login (#5)
    * login process
    
    * add spinner
    
    * implemented loging mechanism
    
    * fix tabs and stepper
    
    * adding DB support
    
    Co-authored-by: Mancuso Antonio <espedito.mancuso@istitutotagnelli.it>
    Co-authored-by: Andrea-Canale <andrea.canale@istitutoagnelli.it>

--- src/pages/ordini.svelte
+++ src/pages/ordini.svelte
@@ -5,5 +5,7 @@
 
 <script>
   import { Page, Navbar, List, ListItem, Block, Button, useStore } from 'framework7-svelte';
+  export let f7router; // this is just to avoid a warning
+  export let f7route;
 </script>
 
