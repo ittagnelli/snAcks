946e5bf34d04be564ce7c958593042b61878bfa0
Mancuso Antonio <espedito.mancuso@istitutotagnelli.it>
2022-04-02 20:57:53 +0200

    fixing issue12

--- src/js/snacks_store.js
+++ src/js/snacks_store.js
@@ -7,6 +7,8 @@ const local_user_login_progress = localStorage.snack_user_login_progress;
 export const user_email = writable(local_user_email || null);
 export const user_authenticated = writable(local_user_authenticated || false);
 export const user_login_progress = writable(local_user_login_progress || false);
+export const last_feedback = writable(0);
+export const title_bar = writable(0);
 
 user_email.subscribe((value) => localStorage.snack_user_email = value);
 user_authenticated.subscribe((value) => localStorage.snack_user_authenticated = value);
