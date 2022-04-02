0fbe7a124cb7c3a959334eef59d3065796a0d399
Andrea Canale <99402143+Andrea-Canale@users.noreply.github.com>
2022-04-02 22:45:29 +0200

    About page (#45)
    fix issue@8

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,8 +2,8 @@ import HomePage from '../pages/home.svelte';
 import StoricoPage from '../pages/storico.svelte';
 import OrdiniPage from '../pages/ordini.svelte';
 import LoginPage from '../pages/login.svelte';
+import AboutPage from '../pages/about.svelte';
 import FeedbackPage from '../pages/feedback.svelte';
-
 var routes = [
   {
     path: '/',
@@ -25,6 +25,10 @@ var routes = [
     path: '/feedback/',
     component: FeedbackPage,
   },
+  {
+    path: '/about/',
+    component: AboutPage,
+  }
 ];
 
 export default routes;
---
+++ src/pages/about.svelte
@@ -0,0 +1,57 @@
+<Page name="about">
+  <Navbar title={$title_bar} />
+  <div>
+    <img src="/icons/144x144.png"/>
+    <h6>{$title_bar}</h6><br>
+
+    <BlockTitle>Applicazione sviluppata per ATS da:</BlockTitle>
+    <Row>
+      <Col width="10"></Col>
+      <Col width="80">
+        <p class="dev">Andrea Canale (3INF)</p>
+        <p class="dev">prof. Antonio Mancuso</p>
+      </Col>
+      <Col width="10"></Col>
+    </Row>
+
+    <!-- <h4>&nbsp;</h4> -->
+    <img src="/ats.png" width="128"/>
+    <!-- <h4>&nbsp;</h4> -->
+    <p>Il codice sorgente è disponibile al seguente <a href="#" on:click={open}>link</a></p>
+
+    <!-- <List accordionList>
+    <ListItem accordionItem title="Privacy Policy">
+      <AccordionContent>
+        <Block>
+          <Policy></Policy>
+        </Block>
+      </AccordionContent>
+    </ListItem>
+</List> -->
+</Page>
+
+<script>
+    import {Page, Block, BlockTitle, Navbar, Link, Button, List, ListItem, AccordionContent, Row, Col} from 'framework7-svelte'
+    import { user_email, last_feedback, title_bar } from '../js/snacks_store.js';
+    // import Policy from '../components/policy.svelte'
+    
+    function open() {
+        window.open("https://github.com/ittagnelli/snAcks")
+    }
+</script>
+
+<style>
+   .dev {
+     color: yellow;
+     font-size: 1.2rem;
+   }
+
+   div {
+     text-align: center;
+   }
+
+   img {
+     margin-top: 40px;
+     margin-bottom: 20px;
+   }
+</style>
