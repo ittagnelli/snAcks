89a9fa3326f0b228ebd115766c6e76c837232704
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2022-04-04 18:12:54 +0200

    manuale.svelte, bar.svelte

---
+++ src/components/bar.svelte
@@ -0,0 +1,10 @@
+<Navbar title={$title_bar}> 
+    <NavRight>
+      <Link iconAurora="f7:info_circle" iconIos="f7:info_circle" iconMd="f7:info_circle" href="/manuale/"/>
+    </NavRight>
+</Navbar>
+
+<script>
+    import {Navbar,NavRight,Link} from 'framework7-svelte'
+    import { user_email, user_authenticated, title_bar } from '../js/snacks_store.js';
+</script>
