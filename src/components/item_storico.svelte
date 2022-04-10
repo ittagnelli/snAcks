ce346b60fc392a30352be66983766fbce861eaab
Antonio Mancuso <mancusoa74@gmail.com>
2022-04-10 10:05:34 +0200

    add storico page and updated manual (#57)
    release of v1.0

---
+++ src/components/item_storico.svelte
@@ -0,0 +1,28 @@
+{#if order.can_delete}
+    <ListItem title="{title_order}">
+        <Button slot="after" color="red" fill onClick={onDeleted}>Rimuovi</Button>
+    </ListItem>
+{:else}
+    <ListItem title="{title_order}"></ListItem>
+{/if}
+    
+<script>
+    import { ListItem, Button, SwipeoutActions, SwipeoutButton } from 'framework7-svelte';
+    import { createEventDispatcher } from 'svelte';
+  
+    const dispatch = createEventDispatcher();
+    export let order;
+
+    let id_order = order.id;
+    let title_order = `Ordine del giorno ${order.order.date_order}`;
+    let date_order = order.order.date_order;
+    
+    console.log("#########");
+    console.log(id_order);
+    console.log(title_order);
+    console.log(date_order);
+
+    function onDeleted() {
+      dispatch('remove', {id: id_order});
+    }
+</script>
--- src/pages/storico.svelte
+++ src/pages/storico.svelte
@@ -1,74 +1,88 @@
-<Page name="storico" onPageTabShow={get_order} class="bg-color-white">
+<Page name="storico" onPageTabShow={get_orders} class="bg-color-white">
   <Nav title={$title_bar} />
-  <BlockTitle>Ultimi 5 ordini</BlockTitle>
-
-  <BlockTitle>Ordine del 4444</BlockTitle>
-  <List>
-    {#each orders as order}
-      <OrderItem order={order} on:remove={onDeleted} />
-    {/each}
-  </List>
-
+  {#if loading == true}
+    <BlockTitle large class="text-align-center">Ricerca ordini</BlockTitle>
+      <div class="preload">
+        <Preloader color="multi" size={60}  />
+      </div>
+    {:else}
+      {#if orders.length > 0}
+        {#each orders as order}
+          <List mediaList>
+            <OrderItem order={order} on:remove={onDeleted} />
+            {#each order.order.food_list as food}
+              <FoodItem type="{food.type}" subtype="{food.subtype}" img="{food.img}" price={food.price} count={food.count} ro={true} />
+            {/each}
+          </List>
+        {/each}
+      {:else}
+      <BlockTitle large class="text-align-center">Non ci sono ordini</BlockTitle> 
+        <div class="no-order-box">
+          <i class="icon f7-icons color-red no-order">multiply_circle_fill</i>
+        </div>
+      {/if}
+    {/if}
 </Page>
-  
 
 <script>
   import { onMount } from 'svelte';
-  import { Badge, f7, Button, Page, Navbar, Block, BlockTitle,Link,NavRight  , List, ListItem, SwipeoutActions, SwipeoutButton} from 'framework7-svelte';
+  import { Preloader, Badge, f7, Button, Page, Navbar, Block, BlockTitle,Link,NavRight  , List, ListItem, SwipeoutActions, SwipeoutButton} from 'framework7-svelte';
   import { get_orders_by_email, delete_doc } from '../js/firebase.js';
   import { user_email, title_bar } from '../js/snacks_store.js';
-  import OrderItem from '../components/list_swipe.svelte';
+  import FoodItem from '../components/food_item.svelte';
+  import OrderItem from '../components/item_storico.svelte';
   import Nav from '../components/bar.svelte'
   
   export let f7router; // this is just to avoid a warning
   export let f7route;
 
-  const HOUR_OF_ORDER = 14;
+  const N_DAYS = 1 * 3600 * 24 * 1000;
   let orders = [];
   // let new_orders;
+  let loading = true;
   onMount(async () => {
 	});
 
-  // const remove_order = (list, id) => list.filter(t => t.id !== id);
-
   async function onDeleted(event) {
-    console.log(event.detail.id);
-    console.log("ORDER PRIMA");
-    console.log(orders);
     await delete_doc('snacks', event.detail.id);
-    // new_orders = orders.filter(t => t.id !== event.detail.id);
-    f7.dialog.alert('Grazie, il tuo ordine è stato rimossod!');
-    
-    console.log("ORDER DOPO");
-    console.log(orders);    
+    loading = true;
+    orders.length = 0;
+    get_orders();
   }
 
-  async function get_order() {
+  async function get_orders() {
     console.log("ENTRO IN STORICO");
-    // orders.length = 0;
-    let raw_orders = await get_orders_by_email($user_email);
-    let current_millis = new Date().getTime();
+    let raw_orders = await get_orders_by_email($user_email);    
+    let current_date = new Date();
 
-    console.log(current_millis);
-    
-    // let current_date = new Date();
-    // let current_hh = current_date.getHours();
-    // current_date.setHours(23);
-    // current_date.setMinutes(59);
-    // current_date.setSeconds(0);
-    // current_date.setMilliseconds(0);
-    // let millis_tomorrow = current_date.getTime() + 60000;
-   
-    console.log(`####### ${raw_orders.length} #########`);
+    orders.length = 0;
+    loading = true;
+
+    current_date.setHours(23);
+    current_date.setMinutes(59);
+    current_date.setSeconds(0);
+    current_date.setMilliseconds(0);
+    let current_millis = current_date.getTime();
     raw_orders.forEach((order) => {
-      order['can_delete'] = (order.order.millis_order - current_millis) > 43200000 ? true : false;
-      // if (current_hh < HOUR_OF_ORDER)
-      //   order['can_delete'] = order.order.millis_order >= millis_tomorrow;
-      // else 
-      //   order['can_delete'] = order.order.millis_order > millis_tomorrow;
+      order['can_delete'] = (order.order.millis_order - current_millis) > N_DAYS ? true : false;
     });
-
     orders = raw_orders;
-    console.log(orders)
+    loading = false;
   }
 </script>
+
+<style>
+  .no-order {
+    font-size: 100px; 
+  }
+
+  .no-order-box {
+    text-align: center;
+    margin: 30px;
+  }
+
+  .preload {
+    text-align: center;
+    padding-top: 100px;
+  }
+</style>
