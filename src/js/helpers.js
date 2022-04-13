dd70370549fda52a61f386f29458cd9f2a6d7280
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2022-04-13 16:26:34 +0200

    fix issue#62

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,10 +1,16 @@
 const N_SKEW_DAYS = 2;
+let festivi = [[14,4],[15,4],[16,4],[18,4],[19,4],[25,4],[2,6]]
 
-//find the next school day
 function find_next_school_day(start) {
+    let festivi_mese = [];
+    festivi.forEach(day => {
+        if(day[1] == start.getMonth()+1){
+            festivi_mese.push(day[0])
+        }
+    });
     do {
       start.setDate(start.getDate() + 1);
-    } while (start.getDay() == 0 || start.getDay() == 6)
+    } while (start.getDay() == 0 || start.getDay() == 6 || festivi_mese.includes(start.getDate(),0))
     return start
 }
   
