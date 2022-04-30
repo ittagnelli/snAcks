87131407c6b7fae40488c419fb3048aa2e33682e
Antonio Mancuso <mancusoa74@gmail.com>
2022-04-30 20:08:15 +0200

    Feature/snacks charts (#84)
    * add snacks-charts dashboard
    
    * fixed favicon and add version
    
    * add number of food items per day and total

--- utils/snAcks-charts/src/order_per_day.svelte
+++ utils/snAcks-charts/src/order_per_day.svelte
@@ -5,8 +5,29 @@
     export let db_orders;
 
 	onMount(async () => {
-        let {labels, values} = orders_per_day(db_orders);
-		chart("#chart_odp", "line", 250, labels, values, "Ordini Giornalieri", ["#ff3e00"]);
+        let {labels_s1, values_s1} = orders_per_day(db_orders);
+        console.log(labels_s1);
+        console.log(values_s1);
+
+
+        let {labels_s2, values_s2} = items_per_day(db_orders); 
+            console.log(labels_s2);
+            console.log(values_s2);
+
+		// chart("#chart_odp", "line", 250, labels, values, "Ordini Giornalieri", ["#ff3e00"]);
+        let s1 = {
+            title: 'Ordini Giornalieri',
+            type: 'line',
+            values: values_s1
+        }
+
+        let s2 = {
+            title: 'Pezzi Giornalieri',
+            type: 'bar',
+            values: values_s2
+        }
+
+        chart("#chart_odp", 'line', 350, "Ordini e Pezzi Giornalieri", ["#ff3e00"], labels_s1, s1, s2);
 	});
 
     function orders_per_day(orders) {
@@ -18,10 +39,27 @@
                 opd[order.date_order] = 1;
         });
 
-        return { labels: Object.keys(opd), 
-                 values: Object.values(opd)
+        return { labels_s1: Object.keys(opd), 
+                 values_s1: Object.values(opd)
                }
     }
+
+    function items_per_day(orders) {
+        let ipd = {};
+        sort_orders(orders).forEach(order => {
+            let n_items = 0;
+            order.food_list.forEach(item => n_items += item.count);
+            if (order.date_order in ipd)
+                ipd[order.date_order] += n_items;
+            else
+                ipd[order.date_order] = n_items;
+        });
+
+        return { labels_s2: Object.keys(ipd), 
+                 values_s2: Object.values(ipd)
+               }
+    }
+
 </script>	
 
 <div class="chart-box" id="chart_odp"></div>
