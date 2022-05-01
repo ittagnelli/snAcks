f4ca3a188815aa3fdcf7c54eeddf385ecaad620c
mancusoa74 <mancusoa74@gmail.com>
2022-05-01 18:33:33 +0200

    add loader spinner and made numbers responsive

--- utils/snAcks-charts/src/number.svelte
+++ utils/snAcks-charts/src/number.svelte
@@ -10,19 +10,18 @@
 
 <style>
     .number-dash {
-        /* width: {width}; */
         margin-right: 12px;
     }
 
     .number {
         font-family: 'Bowlby One SC', cursive;
-        font-size: 64px;
+        font-size: clamp(6vmin, 7vmin, 8vmin);
     }
 
     .title {
         position: relative;
-        top: 6px;
-        font-size: 12px;
+        top: 0.5vw;
+        font-size: clamp(2vmin, 2.5vmin, 3vmin);
         color: rgb(102, 102, 102);
     }
 </style>
