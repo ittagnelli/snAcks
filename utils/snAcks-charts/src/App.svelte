91b4b7b267a54d9b87a813505a99de79d37e5a22
Antonio Mancuso <mancusoa74@gmail.com>
2022-05-01 18:36:34 +0200

    Feature/snacks charts (#85)
    * add snacks-charts dashboard
    
    * fixed favicon and add version
    
    * add number of food items per day and total
    
    * add loader spinner and made numbers responsive

--- utils/snAcks-charts/src/App.svelte
+++ utils/snAcks-charts/src/App.svelte
@@ -6,6 +6,8 @@
 	import Number from './number.svelte';
 	import { total_orders, total_users, total_items } from './chart_helpers';
 
+  import { Circle3 } from 'svelte-loading-spinners'
+
 	//dev
 	// let orders = JSON.parse(localStorage.orders);
 	
@@ -20,7 +22,8 @@
 </script>	
 
 <main>
-	<h1>snAcks Charts v1.1</h1>
+	<h1>snAcks Charts v1.1.1</h1>
+
 	
 	{#if orders.length > 0}
 		<div class="line-of-numbers">
@@ -32,7 +35,10 @@
 		<OrderPerDay db_orders={orders} />	
 		<FoodDivision db_orders={orders} />	
 	{:else}
-		<h2>Impossibile recuperare i dati!!!</h2>
+		<div class="spinner">
+			<Circle3 size="160" color="#FF3E00" unit="px" duration="2s"></Circle3>
+		</div>
+		<h2>Recupero Dati in corso!!!</h2>	
 	{/if}
 
 	
@@ -50,19 +56,26 @@
 	h1 {
 		color: #c10008;
 		text-transform: uppercase;
-		font-size: 2em;
+		font-size: 5vmin;
 		font-weight: bolder;
+		margin-bottom: 30px;
 	}
 
 	h2 {
-		border: 40px solid red;
+		border: 10px solid green;
 		text-transform: uppercase;
 		padding: 40px;
-		width:80%;
+		width:40%;
 		margin: auto;
 		margin-top: 50px;
-		font-size: 2em;
+		font-size: 1.5em;
 		font-weight: bolder;
+	}
+
+	.spinner {
+		display: flex;
+		justify-content: center;
+		margin-top: 20px;
 	}
 
 	.line-of-numbers {
