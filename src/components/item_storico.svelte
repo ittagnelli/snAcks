e638f1ea30dd081912acf1c18b13df3d97ee5254
mancusoa74 <mancusoa74@gmail.com>
2022-05-02 21:37:12 +0200

    fixing issue#88

--- src/components/item_storico.svelte
+++ src/components/item_storico.svelte
@@ -9,14 +9,14 @@
 <script>
     import { ListItem, Button, SwipeoutActions, SwipeoutButton } from 'framework7-svelte';
     import { createEventDispatcher } from 'svelte';
-  
+    
     const dispatch = createEventDispatcher();
     export let order;
-
+    
     let id_order = order.id;
     let title_order = `Ordine del giorno ${order.order.date_order}`;
     let date_order = order.order.date_order;
-    
+            
     console.log("#########");
     console.log(id_order);
     console.log(title_order);
